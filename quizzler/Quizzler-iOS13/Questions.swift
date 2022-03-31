f00dc9a29aeea662341cc30500c6a400af94e468
Danny Bimma <82052316+DannyBimma@users.noreply.github.com>
2022-03-31 11:47:09 -0400

    Added questions struct
    The 2d array that was used to store the questions and their answers was replaced with a Questions struct instead. Functionality remains the same.

---
+++ quizzler/Quizzler-iOS13/Questions.swift
@@ -0,0 +1,14 @@
+//
+//  Questions.swift
+//  Quizzler-iOS13
+//
+//  Created by Daniel Trotman on 31/03/2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Questions {
+    let question: String
+    let answer: String
+}
